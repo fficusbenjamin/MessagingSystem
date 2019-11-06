5a152d0349db7c221287f2c716e832be5dc3c0b3
Francesco Fico <fficusbenjamin@gmail.com>
2019-11-06 23:27:44 +0000

    Improved

--- SoftEngCoursework/MessageWin.xaml.cs
+++ SoftEngCoursework/MessageWin.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Data;
 using MessageObject;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 
@@ -26,6 +27,8 @@ namespace SoftEngCoursework
 
         private void validateEntry()
         {
+            System.Text.RegularExpressions.Regex rID = new System.Text.RegularExpressions.Regex(@"^[SET]");
+
             try
             {
                 isEntryValid = true;
@@ -33,6 +36,11 @@ namespace SoftEngCoursework
                 {
                     isEntryValid = false;
                     throw new ArgumentException("Field cannot be blank", "Message Header/ID");
+                } else
+                if (!rID.IsMatch(_hdrTxt.Text)) 
+                {
+                    isEntryValid = false;
+                    throw new ArgumentException("ID should start with S,E or T", "Message Header/ID");
                 }
 
                 idInput = _hdrTxt.Text;
@@ -53,18 +61,48 @@ namespace SoftEngCoursework
             {
                 case "S":
                     factory = new SmsFactory(typeChoice);
+                    //System.Console.WriteLine("SMS");
                     break;
                 case "E":
                     factory = new EmailFactory(typeChoice);
+                    //System.Console.WriteLine("Email");
                     break;
                 case "T":
                     factory = new TweetFactory(typeChoice);
+                    //System.Console.WriteLine("Tweet");
                     break;
                 default:
                     break;
             }
+
+            Message message = factory.GetMessageType();
+            message.ID = idInput;
+            message.MessageText = bodyInput;
+            sendMessage.add(message);
+            System.Console.WriteLine(typeChoice + "arrivato fino a qui");
         }
 
+        
+
+        private void _hdrTxt_GotFocus(object sender, RoutedEventArgs e)
+        {
+            _hdrTxt.Clear();
+        }
+
+        private void _hdrTxt_LostFocus(object sender, RoutedEventArgs e)
+        {
+           // _hdrTxt.Undo();
+        }
+
+        private void _bdyTxt_LostFocus(object sender, RoutedEventArgs e)
+        {
+            //_bdyTxt.Undo();
+        }
+
+        private void _bdyTxt_GotFocus(object sender, RoutedEventArgs e)
+        {
+            _bdyTxt.Clear();
+        }
 
         private void _bckBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -78,9 +116,13 @@ namespace SoftEngCoursework
             validateEntry();
             if (isEntryValid == true)
             {
-                //addMessage();
+                addMessage();
+                System.Console.WriteLine(_sendMessage.createFile());
             }
         }
+
+
+        
 
 
     }
